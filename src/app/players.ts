b9d029311d7864d7fca2696fb30bbbc97807c10f
Javier <javier2mil8@gmail.com>
2023-07-04 22:32:10 -0300

    added los pibes

--- src/app/players.ts
+++ src/app/players.ts
@@ -7,6 +7,38 @@ const players = [
     runs: 95,
     pubs: 96,
   },
+  {
+    id: 2,
+    name: 'Emma',
+    imagePath: 'assets/images/team/emma.png',
+    beers: 80,
+    runs: 80,
+    pubs: 99,
+  },
+  {
+    id: 3,
+    name: 'Javi',
+    imagePath: 'assets/images/team/javi.png',
+    beers: 70,
+    runs: 80,
+    pubs: 70,
+  },
+  {
+    id: 4,
+    name: 'Eneas',
+    imagePath: 'assets/images/team/eneas.png',
+    beers: 90,
+    runs: 96,
+    pubs: 80,
+  },
+  {
+    id: 5,
+    name: 'Huguito',
+    imagePath: 'assets/images/team/huguito.png',
+    beers: 70,
+    runs: 80,
+    pubs: 70,
+  },
 ];
 
 export default players;
